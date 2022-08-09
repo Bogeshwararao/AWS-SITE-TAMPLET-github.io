3ce7006f9a3f948e39ca8d3099be5369a060d06b
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-08-09 09:37:59 +0530

    nav updates

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import '../components/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WELCOME SANTHOSH
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
     </div>
   );
 }
