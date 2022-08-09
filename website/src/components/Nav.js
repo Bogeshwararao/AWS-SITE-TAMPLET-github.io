7d7bab203c8fc00c083bea3091d13ef47a7c7edd
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-08-09 15:59:52 +0530

    commit

--- website/src/components/Nav.js
+++ website/src/components/Nav.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-  function Nav() {
+export const Nav = () => {
   return (
-    <nav className='nav'><ul>
-        <li>Home</li>
-        <li>Blogs</li>
-        <li>Explore</li>
-        <li>Contacts</li>
-
-        </ul>
-        </nav>
+    <nav className='nav'>
+      <ul>
+      <li>Home</li>
+    <li>Blogs</li>
+    <li>Explore</li>
+    <li>Contacts</li>
+      </ul>
+    </nav>
   )
 }
 export default Nav;
