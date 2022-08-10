c2effb5e219470c075fca9a9da8728d9f5af53fb
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-08-10 21:56:29 +0530

    commit

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Nav from './components/Nav';
+import Nav from './Nav';
 import './App.css';
 
 export default function App() {
---
+++ website/src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav className='nav'>
+    <ul>
+    <li className='lis'>Home</li>
+    <li className='lis'>Blogs</li>
+    <li className='lis'>Explore</li>
+    <li className='lis'>Contacts</li>
+      </ul>
+    </nav>
+  )
+}
+export default Nav;
